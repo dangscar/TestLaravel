8c42e63a61e59496379124a5e53275234392409d
dangrtx <dangrtx@gmail.com>
2024-11-25 14:11:33 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('get-route', function() {
+    return;
+});
+
+Route::post('post-route', function() {
+    return;
+});
+
+Route::put('put-route', function() {
+    return;
+});
+
+Route::patch('patch-route', function() {
+    return;
+});
+
+Route::delete('delete-route', function() {
+    return;
+});
+
+Route::fallback(function() {
+    return "Ooops we couldn't find page";
+});
+
